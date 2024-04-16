3f5e6ea9152dc1d373b4aca9f6d4e576315f1807
bduong02 <65621073+bduong02@users.noreply.github.com>
2024-04-16 01:50:25 -0700

    Update 3-Assn-Shade3dLetter.cpp

--- 3-Assn-Shade3dLetter.cpp
+++ 3-Assn-Shade3dLetter.cpp
@@ -1,4 +1,10 @@
-// RotateLetter.cpp: rotate letter with mouse
+// Bryan Duong
+// 3-Assn-Shade3dLetter.cpp
+// This program produces a shaded image of the
+// letter D using 3D vertices, lighting and the functionality
+// from the Camera class. Additionally, it also allows
+// users to enable/disable the highlights.
+// 4/16/24
 
 #include <glad.h>
 #include <GLFW/glfw3.h>
@@ -6,150 +12,224 @@
 #include "Text.h"
 #include "GLXtras.h"
 #include "VecMat.h"
+#include "IO.h"
+#include "Camera.h"
+#include <iostream>
 
-// a triangle (3 2D locations, 3 RGB colors)
-// for letter D
-vec2 points[] = {
-{150, 230}, {50, 50}, {150, 75}, {220, 110}, {250, 150},
-{250, 270}, {230, 310}, {170, 350}, {50, 350}, {50, 230} };
+// display
+int winWidth = 400, winHeight = 400;					// window size, in pixels
+vec3 mouseWas, mouseNow;								// rotation control
+mat4 standardizeMat;									// scale/center letter
 
+vec3 points[] = { {150, 230}, {50, 50}, {150, 75}, {220, 110}, {250, 150},
+{250, 270}, {230, 310}, {170, 350}, {50, 350}, {50, 230}, 
 
-const int nPoints = sizeof(points) / sizeof(vec2);
-vec3 colors[] = { {0, 0, 1}, {1, 0, 0}, {0, 1, 0} };
+{150, 230, -50}, {50, 50, -50}, {150, 75, -50}, {220, 110, -50}, {250, 150, -50},
+{250, 270, -50}, {230, 310, -50}, {170, 350, -50}, {50, 350, -50}, {50, 230, -50} }; // 3D vertices for the letter D
+
+vec3 colors[] = { {0, 0, 1}, {1, 0, 0}, {0, 1, 0},
+	{0, 0, 1}, {1, 0, 0}, {0, 1, 0}, {0.5, 0.5, 0.5} };	// colors for vertices
+
+const int nPoints = sizeof(points) / sizeof(vec3);
 
 // vertex indices of triangles
 int triangles[][3] = { {0,1,2}, {0,2,3}, {0,3,4}, {0,4,5},
-{0,5,6}, {0,6,7}, {0,7,8}, {0,8,9}, {0, 9, 1} };
+{0,5,6}, {0,6,7}, {0,7,8}, {0,8,9}, {0, 9, 1},
+
+{10, 12, 11}, {10, 13, 12}, {10, 14, 13}, {10, 15, 14},
+	{10, 16, 15}, {10, 17, 16}, {10, 18, 17}, {10, 19, 18}, {10, 11, 19},
+
+	{1, 9, 11}, {11, 9, 19}, {2, 1, 11}, {12, 2, 11}, {3, 2, 12}, {13, 3, 12},
+	{4, 3, 13}, {14, 4, 13}, {5, 4, 14}, {15, 5, 14}, {6, 5, 15}, {16, 6, 15},
+	{7, 6, 16}, {17, 7, 16}, {8, 7, 17}, {18, 8, 17}, {9, 8, 18}, {19, 9, 18}
+};
 
 // OpenGL IDs for vertex buffer and shader program
 GLuint vBuffer = 0, program = 0;
 
-// display
-int winWidth = 400, winHeight = 400;					// window size, in pixels
-vec2 mouseWas, mouseNow;								// rotation control
-mat4 standardizeMat;									// scale/center letter
+// Cameras used to view
+Camera camera(0, 0, winWidth, winHeight, vec3(15, -30, 0), vec3(0, 0, -5), 30);
 
 // Shaders
+// bool variable to track whether highlights are on or not
+bool onHighlights = true;
 
-const char *vertexShader = R"(
+// Vertex shader
+const char* vertexShader = R"(
 	#version 130
-	in vec2 point;
+	in vec3 point;
 	in vec3 color;
 	out vec4 vColor;
-	uniform mat4 view;
+	out vec3 vPoint;
+	uniform mat4 modelview, persp;
 	void main() {
-		gl_Position = view*vec4(point, 0, 1);			// promote point to 4D, transform by view
-		vColor = vec4(color, 1);
+		// transforming vertex to world space
+		vPoint = (modelview*vec4(point, 1.0)).xyz; // transformed to world space
+		// transforming vertex to perspective space
+		gl_Position = persp*vec4(vPoint, 1.0); // transformed to perspective space
+		// passing color to pixel shader
+		vColor = vec4(color, 1.0);
 	}
 )";
 
-const char *pixelShader = R"(
+const char* pixelShader = R"(
 	#version 130
 	in vec4 vColor;
+	in vec3 vPoint;
 	out vec4 pColor;
+
+	uniform vec3 light = vec3(1.0, 1.0, 1.0);
+	uniform float amb = .1, dif = .8, spc =.7; // ambient, diffuse, specular weights
+	uniform bool onHighlights;
 	void main() {
-		pColor = vColor;
+		vec3 dx = dFdx(vPoint); // vPoint change along hor/vert raster
+		vec3 dy = dFdy(vPoint); // vPoint change along hor/vert raster
+		vec3 N = normalize(cross(dx, dy)); // unit-length surface normal
+		vec3 L = normalize(light - vPoint); // unit-length light vector
+		float d = abs(dot(N, L)); // diffuse term
+
+		vec3 E = normalize(vPoint);
+
+		vec3 R = reflect(L, N); // reflection vector
+		float h = max(0, dot(R, E)); // highlight term
+		float s = pow(h, 100); // specular term
+
+		float intensity = min(1, amb+ dif * d) + spc * s; // weighted sum
+		if(onHighlights) {
+			intensity += spc * s;
+		}
+		pColor = vec4(intensity * vColor.rgb, 1); // opaque
 	}
 )";
 
 // Display
 
 void Display() {
-	glClearColor(0,0,0,1);//1, 1, 1, 1);
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(0, 0, 0, 1);//1, 1, 1, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(program);
+
 	// set transform
-	mat4 view = RotateY(mouseNow.x)*RotateX(mouseNow.y)*standardizeMat;
-	SetUniform(program, "view", view);
+	mat4 view = RotateY(mouseNow.x) * RotateX(mouseNow.y) * standardizeMat;
+	SetUniform(program, "modelview", camera.modelview);
+	SetUniform(program, "persp", camera.persp);
+
 	// connect GPU buffer to vertex shader
 	glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
-	VertexAttribPointer(program, "point", 2, 0, (void *) 0);
-	VertexAttribPointer(program, "color", 3, 0, (void *) sizeof(points));
+	VertexAttribPointer(program, "point", 3, 0, (void*)0);
+	VertexAttribPointer(program, "color", 3, 0, (void*)sizeof(points));
+
 	// render
-	glDrawElements(GL_TRIANGLES, sizeof(triangles)/sizeof(int), GL_UNSIGNED_INT, triangles);
+	glDrawElements(GL_TRIANGLES, sizeof(triangles) / sizeof(int), GL_UNSIGNED_INT, triangles);
+
 	// test connectivity
 	UseDrawShader(view);
-	for (int i = 0; i < nPoints; i++) {
-		vec2 p = points[i];
-		Disk(p, 8, vec3(1, 1, 0));
-		Text(vec3(p.x, p.y, 0), view, vec3(1, 1, 0), 10, " v%i", i);
-	}
-	int nTriangles = sizeof(triangles)/(3*sizeof(int));
-	for (int i = 0; i < nTriangles; i++) {
-		int3 t = triangles[i];
-		vec2 p1 = points[t.i1], p2 = points[t.i2], p3 = points[t.i3], c = (p1+p2+p3)/3;
-		Line(p1, p2, 1, vec3(0, 1, 1));
-		Line(p2, p2, 1, vec3(0, 1, 1));
-		Line(p3, p1, 1, vec3(0, 1, 1));
-		Text(vec3(c.x, c.y, 0), view, vec3(0, 1, 1), 10, "t%i", i);
-	}
+	glDisable(GL_DEPTH_TEST);
+	if (!Shift() && camera.down)
+		camera.arcball.Draw(Control());
 	glFlush();
 }
 
 // Mouse Callbacks
-
 void MouseButton(float x, float y, bool left, bool down) {
 	if (left && down)
-		mouseWas = vec2(x, y);
+		camera.Down(x, y, Shift(), Control());
+	else camera.Up();
 }
 
 void MouseMove(float x, float y, bool leftDown, bool rightDown) {
 	if (leftDown) {
-		vec2 m(x, y);
-		mouseNow += (m-mouseWas);
-		mouseWas = m;
+		camera.Drag(x, y);
 	}
+}
+
+void MouseWheel(float spin) {
+	camera.Wheel(spin, Shift());
 }
 
 // Initialization
 
 void StandardizePoints(float s = 1) {
 	// scale and offset so points in +/-s, centered at origin
-	vec2 min, max;
+	vec3 min, max;
 	float range = Bounds(points, nPoints, min, max);
-	float scale = 2*s/range;
-	vec2 center = (min+max)/2;
+	float scale = 2 * s / range;
+	vec3 center = (min + max) / 2;
 	for (int i = 0; i < nPoints; i++)
-		points[i] = scale*(points[i]-center);
+		points[i] = scale * (points[i] - center);
 }
 
 mat4 StandardizeMatrix(float s = 1) {
-	vec2 min, max;
-	float f = 2*s/Bounds(points, nPoints, min, max);
-	return Scale(f)*Translate(-vec3((min+max)/2));
+	vec3 min, max;
+	float f = 2 * s / Bounds(points, nPoints, min, max);
+	return Scale(f) * Translate(-vec3((min + max) / 2));
 }
 
-void BufferVertices(vec2 *points, vec3 *colors, int npoints) {
+void BufferVertices(vec3* points, vec3* colors, int npoints) {
 	// create GPU buffer, make it active
 	glGenBuffers(1, &vBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
 	// allocate memory
-	int sPoints = npoints*sizeof(vec2), sColors = npoints*sizeof(vec3);
-	glBufferData(GL_ARRAY_BUFFER, sPoints+sColors, NULL, GL_STATIC_DRAW);
+	int sPoints = npoints * sizeof(vec3), sColors = npoints * sizeof(vec3);
+	glBufferData(GL_ARRAY_BUFFER, sPoints + sColors, NULL, GL_STATIC_DRAW);
 	// copy to sub-buffers
 	glBufferSubData(GL_ARRAY_BUFFER, 0, sPoints, points);
 	glBufferSubData(GL_ARRAY_BUFFER, sPoints, sColors, colors);
+}
+
+// resize callback
+void Resize(int width, int height) {
+	glViewport(0, 0, width, height);
+	camera.Resize(width, height);
+}
+
+// switches the mode of the highlights
+void ToggleHighlights() {
+	onHighlights = !onHighlights;
+	if (onHighlights) {
+		std::cout << "Highlights are on." << endl;
+	}
+	else {
+		std::cout << "Highlights are off." << endl;
+	}
+}
+
+void KeyCallback(GLFWwindow* w, int key, int scancode, int action, int mods) {
+	// Highlights will be toggled on/off when the user presses H on the keyboard
+	if (key == GLFW_KEY_H && action == GLFW_PRESS) {
+		ToggleHighlights();
+	}
 }
 
 // Application
 
 int main() {
 	// init window
-	GLFWwindow *w = InitGLFW(100, 100, winWidth, winHeight, "Rotate Letter");
+	GLFWwindow* w = InitGLFW(100, 100, winWidth, winHeight, "Rotate Letter");
 	program = LinkProgramViaCode(&vertexShader, &pixelShader);
 	// fit letter to window
-//	StandardizePoints(.8f);						// option: normalize size, center letter
+	Standardize(points, nPoints, .8f);
 	standardizeMat = StandardizeMatrix(.8f);	// option: use matrix to normalize and center
+	
 	// copy vertices to GPU memory
 	BufferVertices(points, colors, nPoints);
+	// register keyboard callback
+	glfwSetKeyCallback(w, KeyCallback);
 	// callbacks and event loop
+	
 	RegisterMouseButton(MouseButton);
 	RegisterMouseMove(MouseMove);
+	RegisterMouseWheel(MouseWheel);
+	RegisterResize(Resize);
+	
 	while (!glfwWindowShouldClose(w)) {
 		Display();
 		glfwSwapBuffers(w);
 		glfwPollEvents();
 	}
+	
 	// finish
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glDeleteBuffers(1, &vBuffer);
